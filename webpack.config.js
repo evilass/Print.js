277f4631c468c9217dab87955c9c4a4792b30355
Hakan Gündüz <777838+evilass@users.noreply.github.com>
2022-07-15 08:15:44 +0200

    update forced

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
-const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -26,62 +24,17 @@ module.exports = {
           loader: 'babel-loader'
         }
       },
-      // TODO: Configure istanbul to interpret how webpack bundles files
-      // {
-      //   test: /\.js$/,
-      //   use: {
-      //     loader: 'istanbul-instrumenter-loader',
-      //     options: { esModules: true }
-      //   },
-      //   enforce: 'post',
-      //   exclude: /node_modules|\.spec\.js$/
-      // },
       {
         test: /\.scss$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              sourceMap: true
-            }
-          },
-          {
             loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
           },
           {
             loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
           }
         ]
       }
     ]
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'print.css'
-    })
-  ],
-  optimization: {
-    minimizer: [
-      new OptimizeCssAssetsPlugin({
-        assetNameRegExp: /\.css$/g,
-        canPrint: false
-      }),
-      new TerserPlugin({
-        cache: false,
-        parallel: true,
-        sourceMap: true, // Must be set to true if using source-maps in production
-        terserOptions: {
-          mangle: true,
-          ie8: true,
-          safari10: true
-        }
-      })
-    ]
-  }
 }
